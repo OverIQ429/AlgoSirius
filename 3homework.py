c1db8a9b172883c803f4b15288ddfb77159be880
admi <maksim.rylkin@mail.ru>
2024-11-08 16:33:44 +0300

    first four

--- 3homework.py
+++ 3homework.py
@@ -6,21 +6,20 @@ class Note:
 class Tree:
     def __init__(self):
         self.root = None
-        self.count = 0
     def check_value(self, root, note):
-        if note.data > root.data:
-            if root.left is None:
-                root.left = note
-            else: self.check_value(root.left, note)
         if note.data < root.data:
             if root.right is None:
                 root.right = note
             else: self.check_value(root.right, note)
+        if note.data > root.data:
+            if root.left is None:
+                root.left = note
+            else: self.check_value(root.left, note)
     def append(self, value):
         node = Note(value)
         if self.root is None:
             self.root = node
-        else:
+        if not self.find(node.data):
             self.check_value(self.root, node)
     def find(self,node, root=None, iteration = 0):
         if root is None:
@@ -29,64 +28,57 @@ class Tree:
         if node == root.data:
             print("Note {} find on {} level of tree".format(node, iteration))
             return root
-        elif node > root.data and root.right is not None:
-            self.find(node, root.right, iteration)
-        elif node < root.data and root.left is not None:
-            self.find(node, root.left, iteration)
+        elif node < root.data and root.right is not None:
+            return self.find(node, root.right, iteration)
+        elif node > root.data and root.left is not None:
+            return self.find(node, root.left, iteration)
         else:
-            return "Error. Not find this Note"
+            return False
 
     def delete(self, node, root=None):
         if root is None:
             root = self.root
-        if node < root.data:
+        if node > root.data:
             root.left = self.delete(node, root.left)
-        elif node > root.data:
+        elif node < root.data:
             root.right = self.delete(node, root.right)
         else:
-            if root.left is None and root.right is None:
+            if root.right is None and root.left is None:
                 return None
-            elif root.left is None:
-                return root.right
             elif root.right is None:
                 return root.left
+            elif root.left is None:
+                return root.right
             else:
-                min_larger_node = self.find_min(root.right)
+                min_larger_node = self.find_min(root.left)
                 root.data = min_larger_node.data
-                root.right = self.delete(min_larger_node.data, root.right)
+                root.left = self.delete(min_larger_node.data, root.left)
         return root
 
     def find_min(self, node):
-        while node.left is not None:
-            node = node.left
+        while node.right is not None:
+            node = node.right
         return node
     def print_tree(self, root=None):
         if root is None:
             root = self.root
         print(root.data)
-        if root.left is not None:
-            print("left from",root.data)
-            self.print_tree(root.left)
         if root.right is not None:
-            print("right from", root.data)
+            print("right from",root.data)
             self.print_tree(root.right)
+        if root.left is not None:
+            print("left from", root.data)
+            self.print_tree(root.left)
 
 BTR = Tree()
 BTR.append(25)
 BTR.append(4)
-BTR.append(7)
+BTR.append(4)
+BTR.append(28)
 BTR.append(2)
 BTR.append(5)
 BTR.append(10)
-BTR.append(79)
-BTR.append(71)
-BTR.append(11)
-BTR.append(12)
-BTR.append(72)
-BTR.append(6)
-BTR.append(36)
-BTR.append(73)
-BTR.append(74)
-BTR.append(4)
+BTR.print_tree()
+BTR.delete(4)
 BTR.print_tree()
 
--- 4homework.py
+++ 4homework.py
@@ -1,158 +1,211 @@
-class Node:
-
-    def __init__(self, data):
-
-        self.data = data
-
+class Note:
+    def __init__(self, date):
+        self.data = date
         self.left = None
-
         self.right = None
-
-
-class BinaryTree:
-
+        self.color = "black"
+        self.parent = None
+class RedBlackTree:
     def __init__(self):
-
         self.root = None
+        self.count = 0
 
+    def change_root(self, new_root, aim):
+        old_root = Note(self.root.data)
+        old_root.color = self.root.color
+        self.root.data = new_root.data
+        self.root.color = new_root.color
+        old_root.left = Note(None)
+        old_root.right = Note(None)
+        if aim == "left":
+            self.root.left = old_root
+        else:
+            self.root.right = old_root
+        old_root.parent = self.root
+
+    def rotate_left(self, note):
+        uncle = note.left
+        father = note.right
+        son = note.right.right
+        if note == self.root:
+            self.change_root(father, "left")
+        else:
+            old_note = note
+            note.data = father.data
+            note.color = father.color
+            note.left = Note(None)
+            note.right = Note(None)
+            note.left = old_note
+        note.right = son
+        note.left.left = uncle
+
+    def rotate_right(self, note):
+        uncle = note.right
+        father = note.left
+        son = note.left.left
+        if note == self.root:
+            self.change_root(father, "right")
+        else:
+            old_note = note
+            note.data = father.data
+            note.color = father.color
+            note.left = Note(None)
+            note.right = Note(None)
+            note.right = old_note
+
+        note.right.right = uncle
+        note.left = son
+
+    def check_child(self, note):
+        if note.parent.color == "red" and note.color == "red":
+            if note.parent == note.parent.parent.right and note.parent.right == note:
+                uncle = note.parent.parent.left
+                if uncle and uncle.color == "black":
+                    note.parent.color = "black"
+                    note.parent.parent.color = "red"
+                    self.rotate_left(note.parent.parent)
+                else:
+                    note.parent.color = "black"
+                    if self.root != note.parent.parent:
+                        note.parent.parent.color = "red"
+                    uncle.color = "black"
+            elif note.parent == note.parent.parent.left and note.parent.left == note:
+                uncle = note.parent.parent.right
+                if uncle and uncle.color == "black":
+                    note.parent.color = "black"
+                    note.parent.parent.color = "red"
+                    self.rotate_right(note.parent.parent)
+            elif note.parent == note.parent.parent.right and note.parent.left == note:
+                tmc = Note(note.data)
+                tmc.color = note.color
+                tmc.left = Note(None)
+                tmc.right = Note(None)
+                father = Note(note.parent.data)
+                father.color = note.parent.color
+                father.left = Note(None)
+                father.right = Note(None)
+                note.parent = tmc
+                note.parent.right = father
+                note.left = None
+                note.right = None
+                note.color = "black"
+                note.data = None
+            elif note.parent == note.parent.parent.left and note.parent.right == note:
+                tmc = Note(note.data)
+                tmc.color = note.color
+                tmc.left = Note(None)
+                tmc.right = Note(None)
+                father = Note(note.parent.data)
+                father.color = note.parent.color
+                father.left = Note(None)
+                father.right = Note(None)
+                note.parent = tmc
+                note.parent.left = father
+                note.left = None
+                note.right = None
+                note.color = "black"
+                note.data = None
+        self.root.color = 'black'
+
+
+    def __rules(self, note):
+        self.check_child(note)
+
+    def check_value(self, root, note, deep=2 ):
+        if note.data > root.data:
+            if root.right and root.right.data is None:
+                note.parent = root
+                root.right = note
+                note.color = "red"
+                null_left = Note(None)
+                note.left = null_left
+                null_right = Note(None)
+                note.right = null_right
+
+            else:
+                deep +=1
+                self.check_value(root.right, note, deep)
+        if note.data < root.data:
+            if root.left and root.left.data is None:
+                note.color = "red"
+                root.left = note
+                note.parent = root
+                null_left = Note(None)
+                note.left = null_left
+                null_right = Note(None)
+                note.right = null_right
+            else:
+                deep += 1
+                self.check_value(root.left, note, deep)
+        self.__rules(note)
+    def append(self, value):
+        node = Note(value)
+        if self.root is None:
+            node.color = "black"
+            null_left = Note(None)
+            node.left = null_left
+            null_right = Note(None)
+            node.right = null_right
+            self.root = node
+            self.count = 2
+        else:
+            self.check_value(self.root, node)
+    def find(self,node, root=None, iteration = 0):
+        if root is None:
+            root = self.root
+        iteration += 1
+        if node == root.data:
+            print("Note {} find on {} level of tree".format(node, iteration))
+            return root
+        elif node > root.data and root.right is not None:
+            self.find(node, root.right, iteration)
+        elif node < root.data and root.left is not None:
+            self.find(node, root.left, iteration)
+        else:
+            return "Error. Not find this Note"
 
     def delete(self, node, root=None):
-
         if root is None:
-
             root = self.root
-
-
-        if root is None:
-
-            return None  # Дерево пустое
-
-
-        # Если значение меньше, идем в левое поддерево
-
         if node < root.data:
-
             root.left = self.delete(node, root.left)
-
-        # Если значение больше, идем в правое поддерево
-
         elif node > root.data:
-
             root.right = self.delete(node, root.right)
-
         else:
-
-            # Узел для удаления найден
-
             if root.left is None and root.right is None:
-
-                # Узел - лист, возвращаем None
-
                 return None
-
             elif root.left is None:
-
-                # Узел с одним правым потомком
-
                 return root.right
-
             elif root.right is None:
-
-                # Узел с одним левым потомком
-
                 return root.left
-
             else:
-
-                # Узел с двумя потомками (необработанный случай)
-
                 min_larger_node = self.find_min(root.right)
-
-                root.data = min_larger_node.data  # Заменяем данные
-
-                root.right = self.delete(min_larger_node.data, root.right)  # Удаляем минимальный узел
-
-
+                root.data = min_larger_node.data
+                root.right = self.delete(min_larger_node.data, root.right)
         return root
 
-
     def find_min(self, node):
-
         while node.left is not None:
-
             node = node.left
-
         return node
+    def print_tree(self, root=None):
+        if root is None:
+            root = self.root
+        print(root.data, "Color is ", root.color )
+        if root.left is not None:
+            print("left from",root.data)
+            self.print_tree(root.left)
+        if root.right is not None:
+            print("right from", root.data)
+            self.print_tree(root.right)
 
+BTR = RedBlackTree()
+BTR.append(25)
+BTR.append(9)
+BTR.append(4)
+BTR.append(28)
+BTR.append(2)
+BTR.append(5)
+BTR.append(10)
+BTR.print_tree()
 
-    def insert(self, data):
-
-        if self.root is None:
-
-            self.root = Node(data)
-
-        else:
-
-            self._insert(data, self.root)
-
-
-    def _insert(self, data, root):
-
-        if data < root.data:
-
-            if root.left is None:
-
-                root.left = Node(data)
-
-            else:
-
-                self._insert(data, root.left)
-
-        else:
-
-            if root.right is None:
-
-                root.right = Node(data)
-
-            else:
-
-                self._insert(data, root.right)
-
-
-    def print_in_order(self, node):
-
-        if node is not None:
-
-            self.print_in_order(node.left)
-
-            print(node.data, end=' ')
-
-            self.print_in_order(node.right)
-
-
-# Пример использования
-
-tree = BinaryTree()
-
-tree.insert(10)
-
-tree.insert(5)
-
-tree.insert(15)
-
-tree.insert(3)
-
-
-print("Дерево до удаления:")
-
-tree.print_in_order(tree.root)  # Вывод: 3 5 10 15
-
-
-# Удаляем узел без детей (лист)
-
-tree.root = tree.delete(3)
-
-
-print("\nДерево после удаления узла 3:")
-
-tree.print_in_order(tree.root)  # Вывод: 5 10 15
