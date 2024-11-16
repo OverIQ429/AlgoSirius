69704ef414056f496ede9d8255db0ee2a398a526
maksi <maksim.rylkin@mail.ry>
2024-11-16 21:22:53 +0300

    last 4

--- 4homework.py
+++ 4homework.py
@@ -20,7 +20,7 @@ class RedBlackTree:
     def rotate_left(self, node):
         right_child = node.right
         node.right = right_child.left
-        if right_child.left is not None:
+        if right_child.left.date is not None:
             right_child.left.parent = node
         right_child.parent = node.parent
         if node.parent is None:
@@ -51,28 +51,30 @@ class RedBlackTree:
         while root != self.root and root.parent.color == "red":
             if root.parent:
                 if root.parent.color == root.color == "red":
-                    if root.parent.parent.left == root.parent:
-                        uncle = root.parent.parent.right
+                    if root.parent.parent:
+                        if root.parent.parent.left == root.parent:
+                            uncle = root.parent.parent.right
+                        else:
+                            uncle = root.parent.parent.left
+                        if uncle.color == "red":
+                            self.case1(root.parent, uncle, root.parent.parent)
+                        else:
+                            if root.parent.parent.left == root.parent and root.parent.right == root:
+                                root = root.parent
+                                self.rotate_left(root)
+                            elif root.parent.parent.right == root.parent and root.parent.left == root:
+                                root = root.parent
+                                self.rotate_right(root)
+                            elif root.parent.parent.left == root.parent and root.parent.left == root:
+                                root.parent.color = "black"
+                                root.parent.parent.color = "red"
+                                self.rotate_right(root.parent.parent)
+                            elif root.parent.parent.right == root.parent and root.parent.right == root:
+                                root.parent.color = "black"
+                                root.parent.parent.color = "red"
+                                self.rotate_left(root.parent.parent)
                     else:
-                        uncle = root.parent.parent.left
-                    if uncle.color == "red":
-                        self.case1(root.parent, uncle, root.parent.parent)
-                    else:
-                        if root.parent.parent.left == root.parent and root.parent.right == root:
-                            root = root.parent
-                            self.rotate_left(root)
-                        elif root.parent.parent.right == root.parent and root.parent.left == root:
-                            root = root.parent
-                            self.rotate_right(root)
-                        elif root.parent.parent.left == root.parent and root.parent.left == root:
-                            root.parent.color = "black"
-                            root.parent.parent.color = "red"
-                            self.rotate_right(root.parent.parent)
-                        elif root.parent.parent.right == root.parent and root.parent.right == root:
-                            root.parent.color = "black"
-                            root.parent.parent.color = "red"
-                            self.rotate_left(root.parent.parent)
-
+                        return
         self.root.color = "black"
 
     def new_value(self, note, root = None):
@@ -92,6 +94,7 @@ class RedBlackTree:
                 self.check_rules(note)
             else:
                 self.new_value(note, root.left)
+
 
 
     def append(self, value):
@@ -118,6 +121,146 @@ class RedBlackTree:
             print("right from", root.date)
             self.print_tree(root.right)
 
+    def find_min(self, node):
+        while node.left.date is not None:
+            node = node.left
+        return node
+
+    def del_v_with_kids(self, node):
+        root = self.find_min(node.right)
+        local_val = node.date
+        node.date = root.date
+        root.date = local_val
+        if root.parent and node.parent:
+            if root.parent.date == root.date:
+                root.parent = node
+        self.delete(root.date, root)
+
+    def del_b_v_with_one_kid(self, node):
+        if node.left.date is not None:
+            local_val = node.date
+            node.date = node.left.date
+            node.left.date = local_val
+            self.delete(node.left.date)
+        else:
+            local_val = node.date
+            node.date = node.right.date
+            node.right.date = local_val
+            self.delete(node.right.date, node.right)
+
+    def del_first_case(self, note, father, brother):
+        note.date = father.date
+        father.date  = brother.date
+        if brother == father.right:
+            note.right = brother.left
+            father.right = brother.right
+            father.right.color = "black"
+        else:
+            note.left = brother.right
+            father.left = brother.left
+            father.left.color = "black"
+
+    def del_second_case(self, note, father, brother):
+        local_val = brother.date
+        brother.date = brother.left.date
+        brother.left = Note(None)
+        brother.left.color = "black"
+        local_val1 = brother.right.date
+        brother.right.date = local_val
+        brother.right.color = "red"
+        brother.right.right.date = local_val1
+        self.del_first_case(note, father, brother)
+
+
+
+    def two_kids_black(self, note, brother,father, node1 = None):
+        brother.color = "red"
+        if father.color == "red":
+            father.color = "black"
+            note.date = None
+            note.left, note.right = None, None
+            note.color = "black"
+        else:
+            if father.date != self.root.date:
+                if father.parent.left == father:
+                    uncle = father.parent.right
+                else:
+                    uncle = father.parent.left
+                self.two_kids_black(father, uncle ,father.parent, note)
+            else:
+                return
+
+    def del_b_v_with_no_kids(self, root):
+        father = root.parent
+        if father is None:
+            return
+        if father.left.date == root.date:
+            brother = father.right
+        else:
+            brother = father.left
+        if brother.color == "black":
+            if brother.left and brother.right:
+                if brother.left.color == "red" or brother.right.color == "red":
+                    if brother.right.color == "red":
+                        self.del_first_case(root, father, brother)
+                    elif brother.right.color == "black":
+                        self.del_second_case(root, father, brother)
+                elif brother.left.color == "black" and brother.right.color == "black":
+                    root.date = None
+                    root.color = "black"
+                    root.left, root.right = None, None
+                    self.two_kids_black(root,brother,father)
+        else:
+            if brother == father.right:
+                local = Note(root.date)
+                local.color = "black"
+                root.date = father.date
+                root.right = brother.left
+                root.color = "red"
+                father.date = brother.date
+                father.right = brother.right
+            else:
+                local = Note(root.date)
+                local.color = "black"
+                root.date = father.date
+                root.left = brother.right
+                root.color = "red"
+                father.date = brother.date
+                father.left = brother.left
+            if root.left.date is None:
+                root.left = local
+                new_val = root.left
+                root.left.left, root.left.right = Note(None), Note(None)
+                root.left.left.color, root.left.right.color = "black", 'black'
+                root.left.parent = root
+            else:
+                root.right = local
+                new_val = root.right
+                root.right.left, root.right.right = Note(None), Note(None)
+                root.right.left.color, root.right.right.color = "black", 'black'
+                root.right.parent = root
+            self.del_b_v_with_no_kids(new_val)
+
+    def delete(self, node, root=None):
+        if root is None:
+            root = self.root
+        if node > root.date:
+            self.delete(node, root.right)
+        elif node < root.date:
+            self.delete(node, root.left)
+        else:
+            if root.color == "red" and root.left.date is None and root.right.date is None:
+                root.date = None
+                root.color = "black"
+                root.left, root.right = None, None
+            elif root.left.date and root.right.date:
+                self.del_v_with_kids(root)
+            elif root.color == "black" and (root.left.date is not None and root.right.date is None or root.right.date is not None and root.left.date is None):
+                self.del_b_v_with_one_kid(root)
+            elif root.color == "black" and root.left.date is None and root.right.date is None:
+                self.del_b_v_with_no_kids(root)
+
+
 BTR = RedBlackTree()
 BTR.append(1)
 BTR.append(2)
@@ -129,4 +272,7 @@ BTR.append(7)
 BTR.append(8)
 BTR.append(9)
 BTR.append(10)
+BTR.delete(7)
+BTR.delete(9)
+BTR.delete(3)
 BTR.print_tree()
